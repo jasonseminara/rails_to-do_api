54e7502bfd7539998919d449f75cf20673879410
Jason Seminara <jseminara@gmail.com>
2016-12-11 20:43:55 -0500

    added user model and token jwt support

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
+  resources :users
+
   patch '/tasks/:id/toggle', to: 'tasks#toggle'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212003709) do
+ActiveRecord::Schema.define(version: 20161212013148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20161212003709) do
     t.boolean  "deleted",     default: false, null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "full_name"
+    t.string   "password_digest"
+    t.string   "token"
+    t.text     "description"
+    t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
 end
